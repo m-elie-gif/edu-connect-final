85b7841f2e5a52d0c29d14b0fda30aee7f57e191
m-elie-gif <ministerelvis94@gmail.com>
2026-08-03 12:52:26 +0300

    Refactor main.dart for EduConnect application

--- lib/main.dart
+++ lib/main.dart
@@ -1,100 +1,63 @@
-// Copyright 2019 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
-import 'package:dual_screen/dual_screen.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/scheduler.dart' show timeDilation;
-import 'package:flutter_gen/gen_l10n/gallery_localizations.dart';
-import 'package:flutter_localized_locales/flutter_localized_locales.dart';
-import 'package:gallery/constants.dart';
-import 'package:gallery/data/gallery_options.dart';
-import 'package:gallery/pages/backdrop.dart';
-import 'package:gallery/pages/splash.dart';
-import 'package:gallery/routes.dart';
-import 'package:gallery/themes/gallery_theme_data.dart';
-import 'package:get_storage/get_storage.dart';
-import 'package:google_fonts/google_fonts.dart';
 
-import 'layout/adaptive.dart';
-
-export 'package:gallery/data/demos.dart' show pumpDeferredLibraries;
-
-void main() async {
-  GoogleFonts.config.allowRuntimeFetching = false;
-  await GetStorage.init();
-  runApp(const GalleryApp());
+void main() {
+  runApp(const EduConnectApp());
 }
 
-class GalleryApp extends StatelessWidget {
-  const GalleryApp({
-    super.key,
-    this.initialRoute,
-    this.isTestMode = false,
-  });
-
-  final String? initialRoute;
-  final bool isTestMode;
+class EduConnectApp extends StatelessWidget {
+  const EduConnectApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ModelBinding(
-      initialModel: GalleryOptions(
-        themeMode: ThemeMode.system,
-        textScaleFactor: systemTextScaleFactorOption,
-        customTextDirection: CustomTextDirection.localeBased,
-        locale: null,
-        timeDilation: timeDilation,
-        platform: defaultTargetPlatform,
-        isTestMode: isTestMode,
+    return MaterialApp(
+      title: 'EduConnect Uganda',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        useMaterial3: true,
       ),
-      child: Builder(
-        builder: (context) {
-          final options = GalleryOptions.of(context);
-          final hasHinge = MediaQuery.of(context).hinge?.bounds != null;
-          return MaterialApp(
-            restorationScopeId: 'rootGallery',
-            title: 'Flutter Gallery',
-            debugShowCheckedModeBanner: false,
-            themeMode: options.themeMode,
-            theme: GalleryThemeData.lightThemeData.copyWith(
-              platform: options.platform,
-            ),
-            darkTheme: GalleryThemeData.darkThemeData.copyWith(
-              platform: options.platform,
-            ),
-            localizationsDelegates: const [
-              ...GalleryLocalizations.localizationsDelegates,
-              LocaleNamesLocalizationsDelegate()
-            ],
-            initialRoute: initialRoute,
-            supportedLocales: GalleryLocalizations.supportedLocales,
-            locale: options.locale,
-            localeListResolutionCallback: (locales, supportedLocales) {
-              deviceLocale = locales?.first;
-              return basicLocaleListResolution(locales, supportedLocales);
-            },
-            onGenerateRoute: (settings) =>
-                RouteConfiguration.onGenerateRoute(settings, hasHinge),
-          );
-        },
-      ),
+      home: const HomeScreen(),
     );
   }
 }
 
-class RootPage extends StatelessWidget {
-  const RootPage({
-    super.key,
-  });
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ApplyTextOptions(
-      child: SplashPage(
-        child: Backdrop(
-          isDesktop: isDisplayDesktop(context),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('EduConnect Uganda'),
+        backgroundColor: Colors.green,
+      ),
+      body: const Center(
+        child: Padding(
+          padding: EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.school,
+                size: 80,
+                color: Colors.green,
+              ),
+              SizedBox(height: 20),
+              Text(
+                'Welcome to EduConnect Uganda!',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Your all-in-one platform connecting schools, teachers, parents, and students across Uganda.',
+                style: TextStyle(fontSize: 16),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
         ),
       ),
     );
